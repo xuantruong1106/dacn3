15ec200fb21aabe04e48532b544266526e500ed9
trường nguyễn xuân <91445722+xuantruong1106@users.noreply.github.com>
2025-01-25 13:28:43 +0700

    Update dashboard.dart

--- lib/user/dashboard.dart
+++ lib/user/dashboard.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import '../database_connect.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DashboardInterface extends StatefulWidget {
   DashboardInterface({super.key});
@@ -79,170 +81,246 @@ class DashboardState extends State<DashboardInterface> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    "Welcome back,",
-                    style: TextStyle(
-                      color: const Color.fromARGB(255, 140, 140, 138),
-                      fontSize: 14,
-                    ),
+                    'Welcome,',
+                    style: GoogleFonts.roboto(textStyle: TextStyle(fontSize: 16,color: Colors.grey)),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 5.0),
-                    child: Text(
-                      "${data[0]['username']}",
-                      style: TextStyle(
-                        color: const Color.fromARGB(255, 37, 37, 37),
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+                  Text(
+                    '${data[0]['username']}',
+                    style: GoogleFonts.roboto(textStyle: TextStyle(fontSize: 20,color: Colors.black)),
                   ),
                 ],
               ),
               Spacer(),
               Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(205, 220, 219, 219),
-                      border: Border.all(
-                        color: Color.fromARGB(205, 232, 231, 231),
-                        width: 1,
-                      ),
-                      borderRadius: BorderRadius.circular(60),
+                padding: const EdgeInsets.only(top: 10),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(205, 220, 219, 219),
+                    border: Border.all(
+                      color: Color.fromARGB(205, 232, 231, 231),
+                      width: 1,
                     ),
-                    child: IconButton(
-                      icon: Icon(
-                        Icons.search,
-                      ),
-                      onPressed: () {
-                        onPressed();
-                      },
-                      color: const Color.fromARGB(255, 0, 0, 0),
+                    borderRadius: BorderRadius.circular(60),
+                  ),
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.search,
                     ),
-                  ))
+                    onPressed: () {
+                      // Implement search functionality here
+                    },
+                    color: const Color.fromARGB(255, 0, 0, 0),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
       ),
-      
       body: SafeArea(
-        child: Container(
-          width: 375.0,
-          height: 200.0,
-          decoration: BoxDecoration(
-             gradient: LinearGradient(
-                colors: [Color.fromARGB(255, 18, 22, 37), const Color.fromARGB(255, 52, 25, 105)], // Adjust the colors as needed
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
+        child: Column(
+          children: [
+            // ATM Card Section
+            Container(
+              width: 375.0,
+              height: 200.0,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(0xFF4B5B98), Color.fromARGB(255, 52, 25, 105)],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+                borderRadius: BorderRadius.circular(20.0),
               ),
-            borderRadius: BorderRadius.circular(20.0)
-          ),
-          margin: const EdgeInsets.only(top: 20.0, left: 20.0),
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 20,
-                  left: 20,
-                  child: Icon(
-                    Icons.credit_card,
-                    color: Colors.white,
-                    size: 30,
-                  ),
-                ),
-                Positioned(
-                  top: 20,
-                  right: 20,
-                  child: Icon(
-                    Icons.contactless,
-                    color: Colors.white,
-                    size: 30,
-                  ),
-                ),
-                Positioned(
-                  top: 70,
-                  left: 20,
-                  child: Text(
-                    '1234 5678 9012 3456', // Card number
-                    style: TextStyle(
+              margin: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 20,
+                    left: 20,
+                    child: Icon(
+                      Icons.credit_card,
                       color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+                      size: 30,
                     ),
                   ),
-                ),
-                Positioned(
-                  top: 110,
-                  left: 20,
-                  child: Text(
-                    'CARDHOLDER NAME', // Cardholder name
-                    style: TextStyle(
+                  Positioned(
+                    top: 20,
+                    right: 20,
+                    child: Icon(
+                      Icons.contactless,
                       color: Colors.white,
-                      fontSize: 14,
+                      size: 30,
                     ),
                   ),
-                ),
-                Positioned(
-                  bottom: 35,
-                  left: 20,
-                  child: Text(
-                    'Expire day', // Expiration date
-                    style: TextStyle(
-                      color: const Color.fromARGB(255, 205, 203, 203),
-                      fontSize: 14,
+                  Positioned(
+                    top: 70,
+                    left: 20,
+                    child: Text(
+                      '1234 5678 9012 3456', // Card number
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-                 Positioned(
-                  bottom: 10,
-                  left: 20,
-                  child: Text(
-                    '12/24', // Expiration date
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
+                  Positioned(
+                    top: 110,
+                    left: 20,
+                    child: Text(
+                      '${data[0]['username']}', // Cardholder name
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
                     ),
                   ),
-                ),
-                Positioned(
-                  bottom: 35,
-                  left: 130,
-                  child: Text(
-                    'CVV', // Expiration date
-                    style: TextStyle(
-                      color: const Color.fromARGB(255, 205, 203, 203),
-                      fontSize: 14,
+                  Positioned(
+                    bottom: 35,
+                    left: 20,
+                    child: Text(
+                      'Expire day', // Expiration date
+                      style: TextStyle(
+                        color: const Color.fromARGB(255, 205, 203, 203),
+                        fontSize: 14,
+                      ),
                     ),
                   ),
-                ),
-                Positioned(
-                  bottom: 10,
-                  left: 130,
-                  child: Text(
-                    '123', // CVV
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
+                  Positioned(
+                    bottom: 10,
+                    left: 20,
+                    child: Text(
+                      '12/24', // Expiration date
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
                     ),
                   ),
-                ),
-                Positioned(
-                  bottom: 20,
-                  right: 20,
-                  child: Image.network(
-                    'https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg',
-                    width: 20,
-                    height: 10,
-                    errorBuilder: (context, error, stackTrace) {
-                      return Icon(
+                  Positioned(
+                    bottom: 35,
+                    left: 130,
+                    child: Text(
+                      'CVV', // Expiration date
+                      style: TextStyle(
+                        color: const Color.fromARGB(255, 205, 203, 203),
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 10,
+                    left: 130,
+                    child: Text(
+                      '123', // CVV
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 10,
+                    right: 30,
+                    child: SvgPicture.asset(
+                      "assets/master-card.svg",
+                      width: 70,
+                      height: 60,
+                      placeholderBuilder: (context) => Icon(
                         Icons.error,
                         color: Colors.red,
-                        size: 40,
-                      );
-                    },
+                        size: 30,
+                      ),
+                    ),
                   ),
+                ],
+              ),
+            ),
+            // Options Section
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 20.0), // Space between the sections
+              child: Container(
+                width: 375.0,
+                height: 100.0,
+                color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Color.fromARGB(205, 232, 231, 231), width: 1),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(Icons.attach_money,
+                              size: 40, color: Colors.black),
+                        ),
+                        Text('Sent', style: TextStyle(color: Colors.black)),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Color.fromARGB(205, 232, 231, 231), width: 1),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(Icons.money_off,
+                              size: 40, color: Colors.black),
+                        ),
+                        Text('Receive', style: TextStyle(color: Colors.black)),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Color.fromARGB(205, 232, 231, 231), width: 1),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(Icons.account_balance,
+                              size: 40, color: Colors.black),
+                        ),
+                        Text('Loan', style: TextStyle(color: Colors.black)),
+                      ],
+                    ),
+                  ],
                 ),
-              ],              
-            ), 
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0, left: 20.0),
+              child: Row(
+                children: [
+                  Text(
+                    'Transaction',
+                    style: GoogleFonts.roboto(
+                      textStyle: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Spacer(),
+                  Padding(padding: const EdgeInsets.only(right: 20.0),
+                  child: Text(
+                    'Sell All',
+                    style: GoogleFonts.roboto(
+                      textStyle: TextStyle(fontSize: 16, color:  Color(0xFF0066FF)),
+                    ),
+                  ),
+                  )
+                  
+                ],
+              ),     
+            ),
+          ],
         ),
       ),
     );
